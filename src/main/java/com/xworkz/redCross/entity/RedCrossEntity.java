c65e766699c56f88a065040b79628494d53e403e
PreethamMR-code <preethampreetham560@gmail.com>
2026-01-09 14:18:16 +0530

    implemented jpa

---
+++ src/main/java/com/xworkz/redCross/entity/RedCrossEntity.java
@@ -0,0 +1,36 @@
+package com.xworkz.redCross.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "donor_account")
+public class RedCrossEntity {
+
+    @Id
+    @Column(name = "donor_account_id")
+    private int donorID;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "dob")
+    private String dob;
+    @Column(name = "zip_code")
+    private String zipCode;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "confirm_password")
+    private String confirmPassword;
+}
